29b856bc38f42511eb4f70ca6a84e1780b547d27
Crista Cooper <aine3d@gmail.com>
2017-10-11 14:00:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,41 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(chocolateBars, bar) {
-  return [bar, ...chocolateBars];
+function addElementToBeginningOfArray(array, element) {
+  [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars, bar) {
-  chocolateBars.unshift(bar);
-  return chocolateBars;
-}
-
-function addElementToEndOfArray(chocolateBars, bar) {
-  return [ ...chocolateBars, bar];
-}
-
-function destructivelyAddElementToEndOfArray(chocolateBars, bar) {
-  chocolateBars.push(bar);
-  return chocolateBars;
-}
-
-function accessElementInArray(array, index) {
-  return (array[index]);
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.shift();
-  return chocolateBars;
-}
-
-function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
-  chocolateBars.pop();
-  return chocolateBars;
-}
-
-function removeElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.slice(1);
-}
-
-function removeElementFromEndOfArray(chocolateBars){
-  return chocolateBars.slice(0, chocolateBars.length-1)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
 }
